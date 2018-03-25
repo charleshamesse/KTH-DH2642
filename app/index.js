54df5719144cda100d6c3e9c70cd96f2667c4eef
charleshamesse <charles.hamesse@gmail.com>
2018-03-25 14:31:37 +0200

    Bootstrap variable overrides

--- app/index.js
+++ app/index.js
@@ -4,7 +4,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 export default class HelloWorld extends React.Component {
   render() {
-     return(<h1>Hello World...How are you</h1>);
+     return(
+     <div>
+       <h1>Hello World...How are you</h1>
+       <a href="#" className="btn btn-primary my-2">Main call to action</a>
+        <a href="#" className="btn btn-secondary my-2">Secondary action</a>
+    </div>);
   }
 }
 ReactDOM.render(
