dd29bece649eb2f88bbe70035e23145e1db1b04d
charleshamesse <charles.hamesse@gmail.com>
2018-03-25 14:45:43 +0200

    Started homepage

--- app/index.js
+++ app/index.js
@@ -5,11 +5,16 @@ import ReactDOM from 'react-dom';
 export default class HelloWorld extends React.Component {
   render() {
      return(
-     <div>
-       <h1>Hello World...How are you</h1>
-       <a href="#" className="btn btn-primary my-2">Main call to action</a>
-        <a href="#" className="btn btn-secondary my-2">Secondary action</a>
-    </div>);
+       <section className="jumbotron text-center">
+            <div className="container">
+              <h1 className="jumbotron-heading">Waaazzaaaaaaa</h1>
+              <p className="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p>
+              <p>
+                <a href="#" className="btn btn-primary my-2 mx-1">Sign Up</a>
+                <a href="#" className="btn btn-secondary my-2 mx-1">Discover Books</a>
+              </p>
+            </div>
+          </section>);
   }
 }
 ReactDOM.render(
