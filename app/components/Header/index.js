8ea0739b2e4cf59f48155b987f6cef9d67fb77e2
charleshamesse <charles.hamesse@gmail.com>
2018-03-29 18:29:54 +0200

    Changed navbar

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -1,67 +1,43 @@
 import React from 'react';
 
 const Header = () => (
-    <header>
-      <div className="collapse bg-secondary" id="navbarHeader">
-        <div className="container">
-          <div className="row">
-            <div className="col-sm-8 col-md-7 py-4">
-              <h4 className="text-white">About</h4>
-              <p className="text-muted">Add some information about the album below, the
-                author, or any other background context. Make it a few sentences long so folks
-                can pick up some informative tidbits. Then, link them off to some social
-                networking sites or contact information.</p>
-            </div>
-            <div className="col-sm-4 offset-md-1 py-4">
-              <h4 className="text-white">Contact</h4>
-              <ul className="list-unstyled">
-                <li>
-                  <a href="#" className="text-white">Follow on Twitter</a>
-                </li>
-                <li>
-                  <a href="#" className="text-white">Like on Facebook</a>
-                </li>
-                <li>
-                  <a href="#" className="text-white">Email me</a>
-                </li>
-              </ul>
-            </div>
-          </div>
+  <header>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-secondary fixed-top">
+      <div className="container">
+        <a className="navbar-brand" href="#">G. Readmore</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="navbar-nav flex-row ml-md-auto d-none d-md-flex" id="navbarNavDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Search</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Bookshelf</a>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                My Account
+              </a>
+              <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <a className="dropdown-item" href="#">Profile</a>
+                <a className="dropdown-item" href="#">Privacy Settings</a>
+                <a className="dropdown-item" href="#">Contact us</a>
+              </div>
+            </li>
+          </ul>
         </div>
+
+
+
       </div>
-      <div className="navbar navbar-dark box-shadow bg-secondary">
-        <div className="container d-flex justify-content-between">
-          <a href="#" className="navbar-brand d-flex align-items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="mr-2">
-              <path
-                d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path>
-              <circle cx="12" cy="13" r="4"></circle>
-            </svg>
-            <strong>G. Readmore</strong>
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarHeader"
-            aria-controls="navbarHeader"
-            aria-expanded="false"
-            aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-        </div>
-      </div>
-    </header>
+    </nav>
+  </header>
 )
 
 export default Header;
