4c4e8918554997fbe91585ee1bb6625c09136d6b
Eysteinn Gunnlaugsson <eysteinngu@gmail.com>
2018-04-17 13:26:36 +0200

    working on having hearts on favorites

--- app/components/BookCard/index.js
+++ app/components/BookCard/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import * as solidHeart from '@fortawesome/fontawesome-free-solid/faHeart'
+import * as emptyHeart from '@fortawesome/fontawesome-free-regular/faHeart'
 
 
 const BookCard = ({ apiId, book, title, thumbnail, isFavorite, addToFavoritesFunc }) => (
   <div className="card mb-4 box-shadow">
+    <div className="card-header">
+      {isFavorite ? <FontAwesomeIcon icon={solidHeart} /> : <FontAwesomeIcon icon={emptyHeart}/> }
+    </div>
     <img className="card-img-top" src={"https://books.google.com/books/content/images/frontcover/" + apiId + "?fife=w300-h450"} alt="Card image cap" />
     <div className="card-body">
       <h4>{title}</h4>
@@ -13,7 +19,6 @@ const BookCard = ({ apiId, book, title, thumbnail, isFavorite, addToFavoritesFun
         <div className="btn-group">
           <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
           <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-          {/* EF  ER TRUE VIL EG GERA TAKKA SEM ER MED ANNAD CLASS */}
           <button type="button" 
                   className={isFavorite ? "btn btn-sm btn-outline-secondary": "btn btn-danger btn-sm btn-outline-secondary"} 
                   onClick={addToFavoritesFunc}>
